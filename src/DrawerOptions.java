c2e22cb6e4327217b4224d12587ca7497e649dca
FronTexas <forfron@gmail.com>
2014-09-21 12:00:37 -0500

    Almost finish the drawer

--- src/DrawerOptions.java
+++ src/DrawerOptions.java
@@ -14,48 +14,36 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.exypnos.R;
 
-public class MainActivity extends ActionBarActivity implements
+public class DrawerOptions extends ActionBarActivity implements
 		OnItemClickListener {
 
 	private DrawerLayout drawerLayout;
 	private ActionBarDrawerToggle drawerListener;
-	private MyAdapterDummy myAdapter;
+	private MyAdapter myAdapter;
 	private ListView listView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.drawer);
+
 		listView = (ListView) findViewById(R.id.drawerList);
-		myAdapter = new MyAdapterDummy(this);
+		myAdapter = new MyAdapter(this);
 		listView.setAdapter(myAdapter);
+
 		listView.setOnItemClickListener(this);
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
 		drawerListener = new ActionBarDrawerToggle(this, drawerLayout,
 				R.drawable.ic_navigation_drawer, R.string.openDrawer,
-				R.string.closeDrawer) {
-			@Override
-			public void onDrawerClosed(View drawerView) {
-				super.onDrawerClosed(drawerView);
-				Toast.makeText(MainActivity.this, "Drawer Closed",
-						Toast.LENGTH_SHORT).show();
-			}
-
-			@Override
-			public void onDrawerOpened(View drawerView) {
-				super.onDrawerOpened(drawerView);
-				Toast.makeText(MainActivity.this, "Drawer Opened",
-						Toast.LENGTH_SHORT).show();
-			}
-		};
+				R.string.closeDrawer);
 		drawerLayout.setDrawerListener(drawerListener);
+
 		getSupportActionBar().setHomeButtonEnabled(true);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 	}
 
 	@Override
@@ -83,32 +71,26 @@ public class MainActivity extends ActionBarActivity implements
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
+		// what does this do???
 		selectItem(position);
 	}
 
-	public void selectItem(int position) {
-		// TODO Auto-generated method stub
+	private void selectItem(int position) {
 		listView.setItemChecked(position, true);
 	}
-
-	public void setTitle(String title) {
-		getSupportActionBar().setTitle(title);
-	}
-
 }
 
-class MyAdapterDummy extends BaseAdapter {
+class MyAdapter extends BaseAdapter {
 	Context context;
 	String[] drawerOptions;
 	int[] drawerIcons = { R.drawable.ic_home, R.drawable.ic_time,
 			R.drawable.ic_settings, R.drawable.ic_mail, R.drawable.ic_calendar,
 			R.drawable.ic_user, R.drawable.ic_app, R.drawable.ic_help };
 
-	public MyAdapterDummy(Context context) {
+	public MyAdapter(Context context) {
 		this.context = context;
 		drawerOptions = context.getResources().getStringArray(
 				R.array.drawerOptions);
-
 	}
 
 	@Override
@@ -117,31 +99,32 @@ class MyAdapterDummy extends BaseAdapter {
 	}
 
 	@Override
-	public Object getItem(int pos) {
-		return drawerOptions[pos];
+	public Object getItem(int position) {
+		return drawerOptions[position];
 	}
 
 	@Override
-	public long getItemId(int pos) {
-		// TODO Auto-generated method stub
-		return pos;
+	public long getItemId(int position) {
+		return position;
 	}
 
 	@Override
-	public View getView(int pos, View convertView, ViewGroup parent) {
+	public View getView(int position, View convertView, ViewGroup parent) {
 		View row = null;
 		if (convertView == null) {
 			LayoutInflater inflater = (LayoutInflater) context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			row = inflater.inflate(R.layout.custom_row, parent, false);
+			inflater.inflate(R.layout.custom_row, parent, false);
 		} else {
 			row = convertView;
 		}
-		TextView titleTextView = (TextView) row.findViewById(R.id.tvDrawerOption);
-		ImageView titleImageView = (ImageView) row
+		TextView tvDrawerOption = (TextView) row
+				.findViewById(R.id.tvDrawerOption);
+		ImageView ivDrawerOption = (ImageView) row
 				.findViewById(R.id.ivDrawerOptions);
-		titleTextView.setText(drawerOptions[pos]);
-		titleImageView.setImageResource(drawerIcons[pos]);
+		tvDrawerOption.setText(drawerOptions[position]);
+		ivDrawerOption.setImageResource(drawerIcons[position]);
 		return row;
 	}
+
 }
